3bcfceafeb195d7b7fca9cbb8426a69685135978
Your Name <you@example.com>
2018-12-06 16:20:02 +0200

    Scaffolded Identity

---
+++ Echange Rates - Course work/src/ExchangeRate.Data/ExchangeRateContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExchangeRate.Data
+{
+    class ExchangeRateContextFactory : IDesignTimeDbContextFactory<ExchangeRateContext>
+    {
+        public ExchangeRateContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                  .Build();
+
+            var builder = new DbContextOptionsBuilder<ExchangeRateContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new ExchangeRateContext(builder.Options);
+        }
+    }
+}
